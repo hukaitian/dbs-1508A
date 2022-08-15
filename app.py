c875851a3d7faa6fec34abca79cbfd4db262446a
hukaitian <111266882+hukaitian@users.noreply.github.com>
2022-08-15 14:24:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 from flask import Flask, render_template, request
+
+
+# In[2]:
+
+
 app = Flask(__name__)
+
+
+# In[3]:
+
+
 import joblib
 
-# @app.route("/", methods = ["GET","POST"])
-# def index():
-#     if request.method == "POST":
-#         rates = float(request.form.get("rates"))
-#         #print(rates)
-#         # model1 = joblib.load("regression_DBS")
-#         # r1 = model1.predict([[rates]])
-#         # model2 = joblib.load('tree_DBS')
-#         # r2 = model2.predict([[rates]]) 
-#         #return (render_template("index.html",result1 = r1, result2 = r2))
-#         return (render_template("index.html",result1 = 'TEMP', result2 = 'TEMP'))
-#     else:
-#         return (render_template("index.html",result1 = "waiting", result2 = "waiting"))
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    if request.method=='POST':
-        rates=float(request.form.get('rates'))
-        model1=joblib.load('regression_DBS')
-        r1=model1.predict([[rates]])
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
         
-        model2=joblib.load('regression_Tree')
-        r2=model2.predict([[rates]])
+        model1 = joblib.load("Regression_DBS")
+        r1 = model1.predict([[rates]])
+        model2 = joblib.load("Tree_DBS")
+        r2 = model2.predict([[rates]])
+        
         return(render_template("index.html",result1=r1,result2=r2))
     else:
         return(render_template("index.html",result1="waiting",result2="waiting"))
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=int('1111'))
+
+# In[ ]:
+
+
+if __name__ == "__main__": # if this program is yours, then run, otherwise, dont run
+    app.run()
+
+
+# In[ ]:
+
+
+
+
